518e0b6cfc4a9c7ea2fac6458ac54840372886a6
mnawalali4 <81225206+mnawalali4@users.noreply.github.com>
2023-01-09 22:41:35 +0500

    Update GeneralInfo.jsx

--- src/screens/Signup/Phases/GeneralInfo.jsx
+++ src/screens/Signup/Phases/GeneralInfo.jsx
@@ -1,26 +1,48 @@
 import React from 'react'
 import { AppHelper } from '../../../helper/AppHelper/AppHelper'
-import { Text, View, StyleSheet, Image } from 'react-native'
+import { Text, View, StyleSheet, Image, TextInput } from 'react-native'
 import { SafeAreaView } from 'react-native-safe-area-context'
 import { images } from '../../../assets'
 
 export const GeneralInfo = ({ navigation }) => {
-    return (
-        <SafeAreaView style={styles.container}>
-            <Image style= {styles.wave} source={images.GeneralInfoWave}></Image>
-            
-        </SafeAreaView>
-    );
+  return (
+    <SafeAreaView style={styles.container}>
+      <Image style={styles.wave} source={images.GeneralInfoWave}></Image>
+
+      <View style={styles.ContentContainer}>
+      <View style={styles.nameContainer}>
+        <TextInput placeholder="First Name" style={styles.name}></TextInput>
+        <TextInput placeholder="Last Name" style={styles.name}></TextInput>
+      </View>
+      </View>
+      
+    </SafeAreaView>
+  )
 }
 
 const styles = StyleSheet.create({
-    container: {
-        flex: 1,
-        alignItems: "center",
-        backgroundColor: AppHelper.material.green50,
-    },
-    wave:{
-        height:"25%",
-        resizeMode:"stretch"
-    }
-});
+  container: {
+    flex: 1,
+    backgroundColor: AppHelper.material.green50,
+  },
+  ContentContainer:{
+paddingStart:10,
+paddingEnd:10
+  },
+  wave: {
+    height: '25%',
+    resizeMode: 'stretch',
+  },
+  nameContainer: {
+    flexDirection: 'row',
+    justifyContent: 'space-evenly',
+  },
+  name: {
+    backgroundColor: 'white',
+    height: 35,
+    width:"45%",
+    borderRadius: 15,
+    borderWidth: 1,
+    paddingStart: 12,
+  },
+})
