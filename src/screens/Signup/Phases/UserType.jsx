d9657e887da75c20420033dca781998bcdeee9b2
Haroon Tahir <noono14252@gmail.com>
2023-01-09 16:11:40 +0500

    Refactoring

--- src/screens/Signup/Phases/UserType.jsx
+++ src/screens/Signup/Phases/UserType.jsx
@@ -17,16 +17,16 @@ export const UserType = ({ navigation }) => {
       <View style={styles.cardholder}>
         
       <TouchableOpacity onPress={() => navigation.navigate("GeneralInfo")}>
-      <View style={styles.card}>
-          <Image style={styles.cardimg} source={images.seller} />
+        <View style={styles.card}>
+          <Image style={styles.cardimg} source={images.Seller} />
           <Text style={styles.cardtextfont}>Seller</Text>
         </View>
       </TouchableOpacity>
 
 
       <TouchableOpacity onPress={() => navigation.navigate("GeneralInfo")}>
-      <View style={styles.card}>
-          <Image style={styles.cardimg} source={images.buyer} />
+        <View style={styles.card}>
+          <Image style={styles.cardimg} source={images.Buyer} />
           <Text style={styles.cardtextfont}>Buyer</Text>
         </View>
       </TouchableOpacity>
