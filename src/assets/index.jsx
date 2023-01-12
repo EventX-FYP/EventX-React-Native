bc21f2d489a940a35693649f72ccd5d73bcfdd55
Haroon Tahir <noono14252@gmail.com>
2023-01-12 16:15:17 +0500

    Login Page

--- src/assets/index.jsx
+++ src/assets/index.jsx
@@ -8,6 +8,8 @@ export const images = {
     Portrait1: { uri: "https://media-public.canva.com/MADFxRUu1Mo/1/thumbnail_large.jpg" },
     React: { uri: "https://reactjs.org/logo-og.png" },
     GoogleIcon: require('./brand-icons/google-icon.png'),
-    Seller: require('./seller.png'),
-    Buyer: require('./buyer.png'),
+    // Seller: require('./seller.png'),
+    // Buyer: require('./buyer.png'),
+    Seller: require('./transparent-icons/87.png'),
+    Buyer: require('./transparent-icons/86.png'),
 };
--- src/screens/Login/Login.jsx
+++ src/screens/Login/Login.jsx
@@ -1,10 +1,11 @@
-import { SafeAreaView, StyleSheet, Image, View } from "react-native";
+import { SafeAreaView, Image, View } from "react-native";
 import { Text, Button } from "react-native-ui-lib";
 import { images } from "../../assets";
 import { AppHelper } from "../../helper/AppHelper/AppHelper";
 import React from "react";
 import { TextField } from "react-native-ui-lib/src/incubator";
 import { StatusBar } from "react-native";
+import { styles } from "./styles";
 
 
 export const Login = ({ navigation }) => {
@@ -13,7 +14,7 @@ export const Login = ({ navigation }) => {
 
     return (
         <SafeAreaView style={styles.container}>
-            <StatusBar />
+            <StatusBar backgroundColor={AppHelper.material.green500} />
             <Image source={images.LoginIcon} style={styles.image} />
             <View style={styles.infoContainer}>
                 <View style={styles.inputContainer}>
@@ -33,84 +34,10 @@ export const Login = ({ navigation }) => {
                     </View>
                     <View style={styles.SignupContainer}>
                         <Text>Don't have an account?  </Text>
-                        <Button label="Signup" onPress={() => navigation.navigate("Signup")} link outline color={AppHelper.material.green400}/>
+                        <Button label="Signup" onPress={() => navigation.navigate("Phases")} link outline color={AppHelper.material.green400}/>
                     </View>
                 </View>
             </View>
         </SafeAreaView>
     );
 }
-
-const styles = StyleSheet.create({
-    inputContainer: {
-        width: "100%",
-        height: "30%",
-        justifyContent: "space-between",
-    },
-    infoContainer: {
-        width: "80%",
-        height: "70%",
-        backgroundColor: AppHelper.material.green50,
-        justifyContent: "space-between",
-    },
-    container: {
-        flex: 1,
-        alignItems: "center",
-        paddingBottom: 20,
-        backgroundColor: AppHelper.material.green50,
-        justifyContent: "space-between"
-    },
-    image: {
-        width: "50%",
-        height: "25%",
-        resizeMode: "stretch",
-    },
-    login: {
-        fontSize: 30,
-        fontWeight: "bold",
-    },
-    googleIcon: {
-        width: 15,
-        height: 15,
-        resizeMode: "stretch",
-    },
-    input: {
-        height: 40,
-        borderWidth: 1,
-        borderColor: AppHelper.material.minBlack,
-        borderRadius: 5,
-        padding: 10,
-    },
-    button: {
-        backgroundColor: AppHelper.material.green600,
-        borderRadius: 5,
-    },
-    googleButton: {
-        backgroundColor: AppHelper.material.white,
-        color: AppHelper.orange,
-        borderRadius: 5,
-    },
-    loginContainer: {
-        width: "100%",
-        height: "40%",
-        justifyContent: "space-between",
-    },
-    buttonContainer: {
-        width: "100%",
-        height: "60%",
-        justifyContent: "space-between",
-    },
-    horizontalLine: {
-        flexDirection: "row",
-        justifyContent: "space-between",
-        alignItems: "center",
-    },
-    SignupContainer: {
-        width: "100%",
-        height: "10%",
-        flexDirection: "row",
-        justifyContent: "center",
-        alignItems: "center",
-    },
-
-});
