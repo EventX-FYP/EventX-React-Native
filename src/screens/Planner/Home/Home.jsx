233e474e68753db87af9ff905887ec4c2015a940
mnawalali4 <81225206+mnawalali4@users.noreply.github.com>
2023-01-25 18:44:15 +0500

    Update Home.jsx

--- src/screens/Planner/Home/Home.jsx
+++ src/screens/Planner/Home/Home.jsx
@@ -2,27 +2,62 @@ import { ScrollView } from 'react-native-gesture-handler'
 import { SafeAreaView } from 'react-native-safe-area-context'
 import { View, Text } from 'react-native-ui-lib'
 import { StyleSheet } from 'react-native'
-import  { JobCard } from '../../../components'
+import { JobCard } from '../../../components'
+
+const TopJobs = "Top Jobs"
+const JobsNear = "Jobs Near You"
+const ReccomendedJobs = "Top Picks For You"
 
 export const Home = ({ navigation }) => {
   return (
-    <SafeAreaView>
-      <ScrollView>
-        <View>
-          <View>
-            <Text>Top Picks For You</Text>
-              <View>
-                <JobCard/>
-          
-              </View>
+    <SafeAreaView >
+      <ScrollView contentContainerStyle={styles.ScrollView}>
+        <View style={styles.ViewHolder}>
+
+          <View style= {styles.Component}>
+            <Text style={styles.heading}>Top Picks For You</Text>
+            <View>
+              <JobCard />
+            </View>
           </View>
+          
+
+          <View style= {styles.Component}>
+            <Text style={styles.heading}>{JobsNear}</Text>
+            <View>
+              <JobCard />
+            </View>
+          </View>
+          
+          <View style= {styles.Component}>
+            <Text style={styles.heading}>{ReccomendedJobs}</Text>
+            <View>
+              <JobCard />
+            </View>
+          </View>
+          
+          
+          
         </View>
       </ScrollView>
     </SafeAreaView>
   )
 }
 
-const Styles = StyleSheet.create({
-  container: {},
-
+const styles = StyleSheet.create({
+  container: {
+    backgroundColor: 'grey',
+    flex: 1,
+  },
+  heading: {
+    fontSize: 25,
+    fontWeight: '400',
+  },
+  ViewHolder:{
+   paddingStart:15,
+  },
+  Component:{
+    justifyContent:"space-evenly", 
+    height:270 
+  }
 })
