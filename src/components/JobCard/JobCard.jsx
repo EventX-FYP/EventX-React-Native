6d0c48850a16356eed003997c4ab4039edae2a84
mnawalali4 <81225206+mnawalali4@users.noreply.github.com>
2023-01-27 22:01:44 +0500

    notifications

--- src/components/JobCard/JobCard.jsx
+++ src/components/JobCard/JobCard.jsx
@@ -4,62 +4,52 @@ import { StyleSheet } from 'react-native'
 import { images } from '../../assets/index'
 import { ScrollView } from 'react-native-gesture-handler'
 import moment from 'moment/moment'
-import { ScreenNavigator, AppHelper } from "../../helper";
+import { ScreenNavigator, AppHelper } from '../../helper'
 
-const currentDate = new Date().toISOString().slice(0, 10)
-const content = "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic"
-export const JobCard = () => {
+export const JobCard = ({ job }) => {
+  // console.log(job)
+  const { title, City, Category, Content, Name, Date } = job?.item
+    ? job.item
+    : job
   return (
+    <View style={styles.CardContainer}>
+      <View style={styles.FirstRow}>
+        <Text style={styles.CardTittle} numberOfLines={1}>
+          {title}
+        </Text>
 
-    
-      <View style={styles.CardContainer}>
-
-        <View style={styles.FirstRow}>      
-            <Text style={styles.CardTittle} numberOfLines={1}>
-              Tittle
-            </Text>
-
-            <Text  style={styles.CardCity} numberOfLines={1}>
-              Lahore
-            </Text>
-        </View>
-
-        <View style={styles.CategoryRow}>
-          <Text numberOfLines={1} style={styles.CategoryText}>
-            Category 1 | Category 1 | Category 1 | Category 1 | Category 1 |
-            Category 1 |
-          </Text>
-        </View>
-
-        <View style={styles.Description}>
-          <Text style={styles.DescriptionContent} numberOfLines={3}>
-             {content}
-          </Text>
-        </View>
-
-        <View style={styles.Row} >
-          <Text style={styles.SecondLastRowHeading} >
-            Posted By:
-          </Text>
-          <Text style={styles.SecondLastRowHeading} >
-            Deadline: 
-          </Text>
-        </View>
-
-        <View style={styles.Row} >
-          <Text numberOfLines={1}  style={styles.SecondLastRowHeading}>
-              Haroon Tahir Haroon Tahir Haroon Tahir Haroon Tahir
-          </Text>
-
-          <Text numberOfLines={1} style={styles.SecondLastRowHeading}>
-          {moment(currentDate).format('DD-MMM-YYYY')}
-          </Text>
-        </View>
-        
+        <Text style={styles.CardCity} numberOfLines={1}>
+          {City}
+        </Text>
       </View>
-    
 
-    
+      <View style={styles.CategoryRow}>
+        <Text numberOfLines={1} style={styles.CategoryText}>
+          {Category}
+        </Text>
+      </View>
+
+      <View style={styles.Description}>
+        <Text style={styles.DescriptionContent} numberOfLines={3}>
+          {Content}
+        </Text>
+      </View>
+
+      <View style={styles.Row}>
+        <Text style={styles.SecondLastRowHeading}>Posted By:</Text>
+        <Text style={styles.SecondLastRowHeading}>Deadline:</Text>
+      </View>
+
+      <View style={styles.Row}>
+        <Text numberOfLines={1} style={styles.SecondLastRowHeading}>
+          {Name}
+        </Text>
+
+        <Text numberOfLines={1} style={styles.SecondLastRowHeading}>
+          {Date}
+        </Text>
+      </View>
+    </View>
   )
 }
 
@@ -70,17 +60,17 @@ const styles = StyleSheet.create({
     borderRadius: 20,
     backgroundColor: AppHelper.material.green50,
     justifyContent: 'space-evenly',
-    paddingHorizontal:15,
-    paddingVertical:5,
-    borderWidth:1
+    paddingHorizontal: 15,
+    paddingVertical: 5,
+    borderWidth: 1,
   },
   CardTittle: {
-    fontWeight:"bold",
+    fontWeight: 'bold',
     maxWidth: '60%',
-    fontSize:15,
+    fontSize: 15,
   },
   FirstRow: {
-    flexDirection:"row",
+    flexDirection: 'row',
     alignItems: 'center',
     justifyContent: 'space-between',
   },
@@ -89,26 +79,26 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'flex-start',
   },
-  Description:{
-    flexDirection:"row",
-    justifyContent:"flex-start",
+  Description: {
+    flexDirection: 'row',
+    justifyContent: 'flex-start',
   },
-  DescriptionContent:{
-    color: AppHelper.gray
+  DescriptionContent: {
+    color: AppHelper.gray,
   },
-  Row:{
-    flexDirection:"row",
-    justifyContent:"space-between"
+  Row: {
+    flexDirection: 'row',
+    justifyContent: 'space-between',
   },
-  SecondLastRowHeading:{
-    maxWidth:"50%",
-    fontWeight:"600"
+  SecondLastRowHeading: {
+    maxWidth: '50%',
+    fontWeight: '600',
   },
-  CategoryText:{
-    color:AppHelper.material.green600,
+  CategoryText: {
+    color: AppHelper.material.green600,
   },
-  CardCity:{
-    fontSize:12,
-    maxWidth:"30%"
-  }
+  CardCity: {
+    fontSize: 12,
+    maxWidth: '30%',
+  },
 })
--- src/screens/Planner/Home/Home.jsx
+++ src/screens/Planner/Home/Home.jsx
@@ -1,47 +1,92 @@
-import { ScrollView } from 'react-native-gesture-handler'
+import { FlatList, ScrollView } from 'react-native-gesture-handler'
 import { SafeAreaView } from 'react-native-safe-area-context'
 import { View, Text } from 'react-native-ui-lib'
 import { StyleSheet } from 'react-native'
 import { JobCard } from '../../../components'
+import moment from 'moment/moment'
 
-const TopJobs = "Top Jobs"
-const JobsNear = "Jobs Near You"
-const ReccomendedJobs = "Top Picks For You"
+const TopJobs = 'Top Jobs'
+const JobsNear = 'Jobs Near You'
+const ReccomendedJobs = 'Top Picks For You'
+
+const currentDate = new Date().toISOString().slice(0, 10)
+
+const JobList = [
+  {
+    id: 1,
+    title: 'tittle',
+    City: 'Lahore',
+    Category:
+      'Category 1 | Category 1 | Category 1 | Category 1 | Category 1 | Category 1 |',
+    Content:
+      "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic",
+    Name: 'Haroon Tahir Haroon Tahir Haroon Tahir Haroon Tahir',
+    Date: moment(currentDate).format('DD-MMM-YYYY'),
+  },
+  {
+    id: 2,
+    title: 'tittle',
+    City: 'Lahore',
+    Category:
+      'Category 1 | Category 1 | Category 1 | Category 1 | Category 1 | Category 1 |',
+    Content:
+      "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic",
+    Name: 'Haroon Tahir Haroon Tahir Haroon Tahir Haroon Tahir',
+    Date: moment(currentDate).format('DD-MMM-YYYY'),
+  },
+  {
+    id: 3,
+    title: 'tittle',
+    City: 'Lahore',
+    Category:
+      'Category 1 | Category 1 | Category 1 | Category 1 | Category 1 | Category 1 |',
+    Content:
+      "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic",
+    Name: 'Haroon Tahir Haroon Tahir Haroon Tahir Haroon Tahir',
+    Date: moment(currentDate).format('DD-MMM-YYYY'),
+  },
+]
 
 export const Home = ({ navigation }) => {
   return (
-    <SafeAreaView >
-      <ScrollView contentContainerStyle={styles.ScrollView}>
+    <SafeAreaView>
+      <ScrollView >
         <View style={styles.ViewHolder}>
-
-          <View style= {styles.Component}>
+          <View style={styles.Component}>
             <Text style={styles.heading}>{TopJobs}</Text>
             <View>
-              <JobCard />
+              <FlatList
+                ItemSeparatorComponent={() => <View style={{ width: 40 }} />}
+                data={JobList}
+                renderItem={({item}) => <JobCard job={item} />}
+                horizontal={true}
+              />
             </View>
           </View>
-          
 
-          <View style= {styles.Component}>
+          <View style={styles.Component}>
             <Text style={styles.heading}>{JobsNear}</Text>
             <View>
-              <ScrollView horizontal={true}>
-              <JobCard />
-              <JobCard />
-              </ScrollView>
-              
+              <FlatList
+                ItemSeparatorComponent={() => <View style={{ width: 40 }} />}
+                data={JobList}
+                renderItem={({item}) => <JobCard job={item} />}
+                horizontal={true}
+              />
             </View>
           </View>
-          
-          <View style= {styles.Component}>
+
+          <View style={styles.Component}>
             <Text style={styles.heading}>{ReccomendedJobs}</Text>
             <View>
-              <JobCard />
+              <FlatList
+                ItemSeparatorComponent={() => <View style={{ width: 40 }} />}
+                data={JobList}
+                renderItem={({item}) => <JobCard job={item} />}
+                horizontal={true}
+              />
             </View>
           </View>
-          
-          
-          
         </View>
       </ScrollView>
     </SafeAreaView>
@@ -57,11 +102,11 @@ const styles = StyleSheet.create({
     fontSize: 25,
     fontWeight: '600',
   },
-  ViewHolder:{
-   paddingStart:15,
+  ViewHolder: {
+    paddingHorizontal: 20,
   },
-  Component:{
-    justifyContent:"space-evenly", 
-    height:270 
-  }
+  Component: {
+    justifyContent: 'space-evenly',
+    height: 270,
+  },
 })
