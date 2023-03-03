ff6372f3dbca92d059b9b58d330f573e7bde2638
mnawalali4 <81225206+mnawalali4@users.noreply.github.com>
2023-03-03 23:36:28 +0500

    job detail changed

--- src/screens/General/JobDetail/JobDetail.jsx
+++ src/screens/General/JobDetail/JobDetail.jsx
@@ -1,8 +1,9 @@
 import React from 'react'
-import { SafeAreaView, Image, View, Text, StyleSheet } from 'react-native'
+import { SafeAreaView, Image, View, Text, StyleSheet, FlatList } from 'react-native'
 import { AppHelper } from '../../../helper'
 import moment from 'moment/moment'
 import { TouchableOpacity } from 'react-native'
+import { Chip } from 'react-native-paper';
 
 const currentDate = new Date().toISOString().slice(0, 10)
 
@@ -13,10 +14,10 @@ const JobList = [
       'tittle tittle tittle tittle tittle tittle tittle tittle tittle tittle',
     City: 'Lahore',
     Category:
-      'Category 1 | Category 1 | Category 1 | Category 1 | Category 1 | Category 1 |',
+      ["category1", "category2", "catgeory3", "category1",],
     Content:
-      "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic",
-    Name: 'Haroon Tahir Haroon Tahir Haroon Tahir Haroon Tahir',
+      "Lorem Ipsum is  Ipsum is simplyLorem Lorem Ipsum is  Ipsum is simplyLorem  Ipsum is si mplyLorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic",
+    Name: 'Haroon Tahir Haroon',
     Date: moment(currentDate).format('DD-MMM-YYYY'),
   },
 ]
@@ -24,38 +25,36 @@ const JobList = [
 export const JobDetail = ({ navigation }) => {
   return (
     <SafeAreaView style={styles.container}>
-      <View style={styles.detailContainer}>
-        <Text style={styles.majorHeading}> {JobList[0].title}</Text>
-        <Text style={styles.category}>{JobList[0].Category}</Text>
-
-        <View style={styles.row}>
-          <Text style={styles.heading}>Posted By</Text>
-          <Text style={styles.heading}>Date</Text>
-        </View>
-
-        <View style={styles.row}>
-          <Text style={styles.name}>{JobList[0].Name}</Text>
+      <View style={styles.row}>
+        <View style={styles.col1}>
+          <Text style={styles.majorHeading}> {JobList[0].title}</Text>
           <Text style={styles.date}>{JobList[0].Date}</Text>
         </View>
-       
 
-        <Text style={styles.heading}>{JobList[0].City}</Text>
+        <View style={styles.col2}>
+        <Text style={styles.name}>{JobList[0].Name}</Text>
+        <Text style={styles.cityHeading}>{JobList[0].City}</Text>
+        </View>
+      
       </View>
 
-      <View style={styles.descriptionContainer}>
-        <Text style={styles.majorHeading}>Description</Text>
+      <View style={styles.chipContainer}>
+        <FlatList
+          data={JobList[0].Category}
+          ItemSeparatorComponent={() => <View style={{ width: 40 }} />}
+          horizontal={true}
+          renderItem={({ item }) => <Chip  textStyle={styles.chipTextStyle} style={styles.chipStyle} >{item}</Chip>}/>
+      </View>
+
+
+        <Text style={styles.descriptionHeading}>Description</Text>
         <Text style={styles.description}>{JobList[0].Content}</Text>
-      </View>
+     
 
-      <View style={styles.buttonContainer}>
-        <TouchableOpacity>
-        <Text style={styles.button}>Go Back</Text>
-        </TouchableOpacity>
-
-        <TouchableOpacity>
+      
+        <TouchableOpacity style={styles.buttonContainer} >
         <Text style={styles.button}>Bid</Text>
         </TouchableOpacity>
-      </View>
     </SafeAreaView>
   )
 }
@@ -64,11 +63,10 @@ const styles = StyleSheet.create({
   container: {
     paddingHorizontal: 10,
     flex: 1,
-    justifyContent: 'space-evenly',
+   marginTop:20
   },
   majorHeading: {
     fontSize: 25,
-    maxWidth: '80%',
   },
   category: {
     fontSize: 16,
@@ -76,22 +74,24 @@ const styles = StyleSheet.create({
   },
   row: {
     flexDirection: 'row',
-    justifyContent: 'space-between',
+    flex:0.5,
+    justifyContent: 'space-between'
   },
-  heading: {
+  cityHeading: {
     fontSize: 18,
   },
   name: {
-    maxWidth: '45%',
+   
     color: AppHelper.gray,
   },
   date: {
-    maxWidth: '45%',
+   
     color: AppHelper.gray,
   },
   description: {
     color: AppHelper.gray,
     fontSize: 15,
+    marginVertical:25
   },
   button:{
     backgroundColor: AppHelper.material.green400,
@@ -100,19 +100,41 @@ const styles = StyleSheet.create({
     fontWeight: 'bold',
     fontSize:20,
     paddingVertical:10,
-    borderRadius:20
-  },
-  detailContainer:{
-    flex: 0.5, 
-    justifyContent: 'space-evenly' 
+    borderRadius:20,
+    width:'90%',
+    textAlign:"center",
   },
   descriptionContainer:{
-    flex: 0.3, 
+     
     justifyContent: 'space-evenly'
   },
   buttonContainer:{
-    flexDirection:"row", 
-    justifyContent: 'space-between', 
-    flex:0.1
+    justifyContent:"center",
+    alignItems:"center"
+  },
+  col1:{
+    maxWidth: '60%',
+    justifyContent: 'space-evenly',
+  },
+  col2:{
+    maxWidth: '30%',
+    alignItems:"center",
+    justifyContent: 'space-evenly' 
+  },
+  descriptionHeading:{
+    fontSize: 25,
+    marginVertical:20
+  },
+  chipContainer:{
+
+  },
+  chipStyle:{
+    backgroundColor:AppHelper.material.green300,
+  },
+  chipTextStyle:{
+    color:"white",
+    fontSize:15,
+
   }
+
 })
